fed52f36de404aa4b7da09a66c68d8ef56d00518
cFalguera <115069089+cFalguera@users.noreply.github.com>
2023-04-14 09:16:43 +0200

    Improve Unlocktime comments

--- scripts/submit-tx/vesting-basic-withdraw.js
+++ scripts/submit-tx/vesting-basic-withdraw.js
@@ -9,22 +9,22 @@ const {ethers} = require("hardhat");
 const GFALTokenArtifact = require("../../artifacts/contracts/_mock/GFALToken.sol/GFALToken.json");
 
 // Constants
-// Constants for - Thu Apr 13 2023 16:00:00 GMT+0200
+// Unlocktime - Thu Apr 13 2023 16:00:00 GMT+0200
 const VESTING_CONTRACTS = [
   "0x5C3227A15a4917590f0Ea114652725D7030B5537", // Game
   "0xD5dcB7b469eF01283B41EFE468593a71BEee48D6", // Marketing
   "0x39cf1cdF5152e851C769Ee4B8c7fFF489B1B36cF" // KOLS
 ]
-// Constants for - Tue Jun 13 2023 16:00:00 GMT+0200 - Private, developers
+// Unlocktime - Tue Jun 13 2023 16:00:00 GMT+0200 - Private, developers
 const VESTING_CONTRACTS_1 = [
   "0x6f4FC00457CAfb8fFF19715A6E38D6Fc386B6857", // Private
   "0xb18771af81eFEd73911Bfe95389F0A28e946592d" // Developers
 ]
-// Constants for - Fri Sep 13 2024 16:00:00 GMT+0200 - Reserve
+// Unlocktime - Fri Sep 13 2024 16:00:00 GMT+0200 - Reserve
 const VESTING_CONTRACTS_2 = [
   "0x8EEDd042caCE47963F52E9D2190c97Ab00F33f03" // Reserve
 ]
-// Constants for - Wed Mar 13 2024 15:00:00 GMT+0100 - Team, advisors, Foundation
+// Unlocktime - Wed Mar 13 2024 15:00:00 GMT+0100 - Team, advisors, Foundation
 const VESTING_CONTRACTS_3 = [
   "0x08E6D346cef70D8F680D520256B75ba504cCB942", // Team
   "0xA46E64D618475ff8379f152A03317a108a89fDd1", // Advisors
